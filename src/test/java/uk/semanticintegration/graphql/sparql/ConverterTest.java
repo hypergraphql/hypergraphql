cabc7b807bd1428ccd090c970f8163b3b9bf0ec8
Philip Coates <philip.coates@sparqlr.com>
2017-10-19 20:22:54 +0100

    Added JUnit5 libs to gradle build file and created stub tests

---
+++ src/test/java/uk/semanticintegration/graphql/sparql/ConverterTest.java
@@ -0,0 +1,10 @@
+package uk.semanticintegration.graphql.sparql;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by philipcoates on 2017-10-19T20:21
+ */
+class ConverterTest {
+
+}
