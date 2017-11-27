921e5470550cbc1be5e31e5d42357adeaf996d8f
Szymon Klarman <szymon.klarman@gmail.com>
2017-11-27 13:15:16 +0000

    Service class restored

--- src/main/java/org/hypergraphql/config/Service.java
+++ src/main/java/org/hypergraphql/config/Service.java
@@ -3,25 +3,37 @@ package org.hypergraphql.config;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public abstract class Service {
+public class Service {
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
+    @JsonCreator
+    public Service(@JsonProperty("@type") String type,
+                         @JsonProperty("@id") String id,
+                         @JsonProperty("url") String url,
+                         @JsonProperty("user") String user,
+                         @JsonProperty("graph") String graph,
+                         @JsonProperty("password") String password
+    ) {
+        this.type = type;
         this.id = id;
-    }
-
-    public String getGraph() {
-        return graph;
-    }
-
-    public void setGraph(String graph) {
+        this.url = url;
         this.graph = graph;
+        this.user = user;
+        this.password = password;
+
     }
 
+    private String type;
     private String id;
+    private String url;
     private String graph;
+    private String user;
+    private String password;
+
+    public String type() { return this.type; }
+    public String id() { return this.id; }
+    public String url() { return this.url; }
+    public String graph() { return this.graph; }
+    public String user() { return this.user; }
+    public String password() { return this.password; }
 
 }
