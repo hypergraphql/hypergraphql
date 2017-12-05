077b3113e2e99a47bcbc6da8dfc7b61fc925f698
Szymon Klarman <szymon.klarman@gmail.com>
2017-12-05 20:26:09 +0000

    minor vocabulary bug fix

--- src/main/java/org/hypergraphql/config/schema/HGQLVocabulary.java
+++ src/main/java/org/hypergraphql/config/schema/HGQLVocabulary.java
@@ -7,7 +7,7 @@ public class HGQLVocabulary {
 
     public static final String HGQL_NAMESPACE = "http://hypergraphql.org/";
     public static final String HGQL_QUERY_URI = HGQL_NAMESPACE + "query";
-    public static final String HGQL_QUERY_PREFIX = HGQL_NAMESPACE + "query/";
+    public static final String HGQL_QUERY_PREFIX = HGQL_QUERY_URI + "/";
 
     public static final Map<String, String> JSONLD_VOC = new HashMap<String, String>() {{
         put("_id", "@id");
