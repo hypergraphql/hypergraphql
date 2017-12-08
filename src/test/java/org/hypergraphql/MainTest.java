a803ce5ab2b884f4e4669bc14b00b14fc96985e8
Mirko Dimartino <dimartinomirko@gmail.com>
2017-12-08 13:31:40 +0000

    first commit

--- src/test/java/org/hypergraphql/MainTest.java
+++ src/test/java/org/hypergraphql/MainTest.java
@@ -1,23 +1,18 @@
 package org.hypergraphql;
 
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+
 public class MainTest {
 
-
-    public static void main(String[] agrs) {
-
-//        Model model = ModelFactory.createDefaultModel();
-//        Resource subject = model.createResource("ciao");
-//        Property predicate = model.createProperty("ciao");
-//        RDFNode object = model.createLiteral("ciao");
-//        model.add(subject,predicate,object);
-//        Model model1 = ModelFactory.createDefaultModel();
-//        Resource subject1 = model1.createResource("ciao");
-//        Property predicate1 = model1.createProperty("ciao");
-//        Iterator<RDFNode> iterator = model.listObjectsOfProperty(predicate1);
-//        while (iterator.hasNext())
-//            System.out.println(iterator.next());
-//
+   @Test
+   public void testtest(){
 
 
-    }
+       assertNull("Test OK",new Object());
+
+   }
+
 }
--- src/test/java/org/hypergraphql/SparqlClientTest.java
+++ src/test/java/org/hypergraphql/SparqlClientTest.java
@@ -18,7 +18,7 @@ import org.apache.jena.sparql.ARQConstants;
 import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 import org.apache.jena.sparql.util.Context;
 import org.apache.jena.sparql.util.Symbol;
-import org.junit.jupiter.api.Test;
+
 
 import java.io.InputStream;
 
@@ -27,28 +27,28 @@ import java.io.InputStream;
  * Created by philipcoates on 2017-10-19T20:22
  */
 class SparqlClientTest {
-
-    @Test
-    public void query_with_auth_11() {
-
-    }
-
-    @Test
-    public void unirestSparqlQuery() throws UnirestException {
-
-//        String queryString = "SELECT * WHERE { ?x ?y ?z } LIMIT 10";
-//        String url = "http://ec2-52-23-193-209.compute-1.amazonaws.com:5820/photobox/query";
-//        String user = "admin";
-//        String password = "admin";
 //
-//        HttpResponse<JsonNode> resp = Unirest.get(url)
-//                .queryString("query", queryString)
-//                .header("Accept", "application/sparql-results+json")
-//                .basicAuth(user, password)
-//                .asJson();
+//    @Test
+//    public void query_with_auth_11() {
 //
-//        System.out.println("Unirest: " + resp.getStatus());
-//        resp.getBody().getObject().getJSONObject("head").getJSONArray("vars").iterator().forEachRemaining(var -> System.out.println(var));
-
-    }
+//    }
+//
+//    @Test
+//    public void unirestSparqlQuery() throws UnirestException {
+//
+////        String queryString = "SELECT * WHERE { ?x ?y ?z } LIMIT 10";
+////        String url = "http://ec2-52-23-193-209.compute-1.amazonaws.com:5820/photobox/query";
+////        String user = "admin";
+////        String password = "admin";
+////
+////        HttpResponse<JsonNode> resp = Unirest.get(url)
+////                .queryString("query", queryString)
+////                .header("Accept", "application/sparql-results+json")
+////                .basicAuth(user, password)
+////                .asJson();
+////
+////        System.out.println("Unirest: " + resp.getStatus());
+////        resp.getBody().getObject().getJSONObject("head").getJSONArray("vars").iterator().forEachRemaining(var -> System.out.println(var));
+//
+//    }
 }
