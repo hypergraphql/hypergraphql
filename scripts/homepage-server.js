0f076c28c787b12e52943559741fc1b0febe8e4c
pc3356 <1263620+pc3356@users.noreply.github.com>
2020-02-05 01:17:23 +0100

    Revert demo url settings

--- scripts/homepage-server.js
+++ scripts/homepage-server.js
@@ -1,26 +1,26 @@
 var config = {
   "port": 8080,
-  "demoServerBase":"http://demo.hypergraphql.org",
+  "demoServerBase":"https://demo.hypergraphql.org",
   "gitpage":"https://hypergraphql.github.io/hypergraphql",
   "gitdomain":"https://hypergraphql.github.io",
   "services": [
     {
-      "port": 8081,
+      "port": 8481,
       "graphql": "graphql1",
       "graphiql": "graphiql1"
     },
     {
-      "port": 8082,
+      "port": 8482,
       "graphql": "graphql2",
       "graphiql": "graphiql2"
     },
     {
-      "port": 8083,
+      "port": 8483,
       "graphql": "graphql3",
       "graphiql": "graphiql3"
     },
     {
-      "port": 8084,
+      "port": 8484,
       "graphql": "graphql4",
       "graphiql": "graphiql4"
     }
